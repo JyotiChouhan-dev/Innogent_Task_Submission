9a40ed9fd392eb8574f403bff27415d2491f2c37
Jyoti Singh Chouhan <jyoti.chouhan@innogent.in>
2025-10-23 21:28:51 +0530

    Update DemoGitSetup and -2

--- DemoGitSetup.java
+++ DemoGitSetup.java
@@ -5,5 +5,7 @@ class DemoGitSetup
 		System.out.println("demo");
 		System.out.println("git");
 		System.out.println("setup");
+		System.out.println("all");
+		System.out.println("done");
 	}
 }
--- DemoGitSetup2.java
+++ DemoGitSetup2.java
@@ -3,5 +3,9 @@ class DemoGitSetup2
 	public static void main(String ar[])
 	{
 		System.out.println("Setup -2");
+		System.out.println("Setup -2");
+		System.out.println("Setup -2");
+		System.out.println("Setup -2");
+		System.out.println("Setup -2");
 	}
 }
